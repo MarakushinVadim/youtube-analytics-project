0a07dd3300c642c49015baa00e3d70d3cec90dd2
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-01-27 23:46:04 +0300

    add API

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('AIzaSyCcHpz8P7XURt_qrMdf6rfgnH5My8KASsM')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
