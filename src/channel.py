092b0e0ffae241b221b42730af733c35165a3ec1
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-01-28 19:42:56 +0300

    get result

--- src/channel.py
+++ src/channel.py
@@ -14,12 +14,64 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-
 
         def printj(dict_to_print: dict) -> None:
             """Выводит словарь в json-подобном удобном формате с отступами"""
             print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-        printj(channel)
+        printj(self.channel)
+
+
+    @property
+    def title(self):
+        video_title: str = self.channel_dict['items'][0]['snippet']['title']
+        return video_title
+
+    @property
+    def url(self):
+        channel_link: str = self.channel_dict['items'][0]['snippet']['thumbnails']['default']['url']
+        return channel_link
+
+    @property
+    def channel_description(self):
+        channel_description: str = self.channel_dict['items'][0]['snippet']['description']
+        return channel_description
+
+    @property
+    def channel(self):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
+
+
+    @property
+    def subscribers_count(self):
+        subscribers_count: str = self.channel_dict['items'][0]['statistics']['subscriberCount']
+        return subscribers_count
+
+
+    @property
+    def video_count(self):
+        video_count: str = self.channel_dict['items'][0]['statistics']['videoCount']
+        return video_count
+
+
+    @property
+    def view_count(self):
+        view_count: str = self.channel_dict['items'][0]['statistics']['viewCount']
+        return view_count
+
+    @property
+    def channel_dict(self):
+        channel_dict: dict = self.channel
+        return channel_dict
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+
+    def to_json(self, file_name):
+        pass
